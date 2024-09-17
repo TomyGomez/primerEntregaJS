bb24cbbe9e1622f572f5f58294f878ad336ceb4b
tomasjoaquingomez1234 <tomasjoaquingomez1234@gmail.com>
2024-09-17 22:23:46 +0000

    primerEntrega1Gomez

--- script.js
+++ script.js
@@ -9,35 +9,39 @@ pensar el alcance de tu proyecto. cotizador de seguros? un simulador de creditos
 
 // condicional y ciclo 
 
-console.log("Promedio de Notas: \n");
+alert("Primer parte de la Entrega \nPromedio de Notas: \n");
 
 let promedio = 0;
 let acumulador = 0;
 let contador= 0;
 
 for(let i = 1; i<=4;i++){
-    let numero = parseInt(prompt("Ingrese una nota:"));
+    let numero = parseInt(prompt("Ingrese la nota del examen numero "+i+":"));
     acumulador += numero;
     contador++;
 
 }
+alert("Porfavor Revise la consola para verificar el Promedio calculado!");
 promedio = acumulador/contador;
 if(promedio >= 6 && promedio<=10){
-    console.log("FELICIDADES! APROBASTE Y TU PROMEDIO ES:" +promedio);
+    console.log("FELICIDADES! Aprobaste y tu promedio es:" +promedio);
 
 }else{
     console.log("Lo lamento Desaprobaste y tu promedio es:"+promedio );
 }
 
 
+//calculadora de impuesto de tarjeta 
+alert("Segundo Ejercicio \nCalculadora de Impuesto Dolar");
 
-console.log("SIGUIENTE EJERCICOS \nCALCULADORA DE IMPUESTO Dolares")//calculadora de impuesto de tarjeta 
-
-let unDolar = 730;
-alert("el dolar esta : "+ unDolar);
+let unDolar = 982;
+alert("el dolar para la venta esta : "+ "$"+ unDolar);
 let monto = parseInt(prompt("Ingrese un valor en dolares:"));
 let dolar = unDolar*monto;
-let impuestoTarjeta = dolar*(65/100) + dolar;
+let impuestoTarjeta = dolar*(38/100) + dolar;
 
-console.log("El monto sin impuestos es : "+ monto*unDolar);
-console.log("Los dolares a abonar con impuesto del 65% es: "+ impuestoTarjeta);
+alert("Porfavor Revise la consola para verificar el valor del Dolar calculado!");
+
+console.log("Los dolares a calcular son: " +"u$"+ monto);
+console.log("El monto sin impuestos es : "+ "$"+ dolar);
+console.log("Los dolares a abonar con impuesto del 38% es: "+ "$"+impuestoTarjeta);
